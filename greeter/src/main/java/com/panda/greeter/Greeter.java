4b81dfb84b6cdb093530fd7d7cc59fb953d8aaf1
Xiaofeng Wang <xfun68@gmail.com>
2013-06-28 12:18:26 +0300

    Greeter greets to person.

--- greeter/src/main/java/com/panda/greeter/Greeter.java
+++ greeter/src/main/java/com/panda/greeter/Greeter.java
@@ -1,8 +1,14 @@
 package com.panda.greeter;
 
+import com.panda.person.Person;
+
 public class Greeter {
   public String greet() {
     return "Hello World";
+  }
+
+  public String greetTo(Person person) {
+    return "Hi, " + person.name() + ".";
   }
 }
 
