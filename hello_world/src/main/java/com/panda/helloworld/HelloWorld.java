2b2a177fe9c2d068dd2b180c03c410fd52d043e6
Xiaofeng Wang <xfun68@gmail.com>
2013-06-28 12:50:51 +0300

    Compile mutiple files in one project.

--- hello_world/src/main/java/com/panda/helloworld/HelloWorld.java
+++ hello_world/src/main/java/com/panda/helloworld/HelloWorld.java
@@ -4,14 +4,16 @@ import com.panda.greeter.Greeter;
 import com.panda.person.Person;
 
 class HelloWorld {
-  public static void main(String[] args) {
+  public String hi() {
+    String result = "";
     Greeter greeter = new Greeter();
-
-    System.out.println(greeter.greet());
-
     Person person = new Person("Obama");
-    String greeting = greeter.greetTo(person);
-    System.out.println(greeting);
+
+    result += greeter.greet();
+    result += "\n";
+    result += greeter.greetTo(person);
+
+    return result;
   }
 }
 
---
+++ hello_world/src/main/java/com/panda/helloworld/Main.java
@@ -0,0 +1,10 @@
+package com.panda.helloworld;
+
+import com.panda.helloworld.HelloWorld;
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println(new HelloWorld().hi());
+  }
+}
+
