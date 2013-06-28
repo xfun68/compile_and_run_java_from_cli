32dd14e6e26cdfd8eb91d0427fa68057866d4a56
Xiaofeng Wang <xfun68@gmail.com>
2013-06-28 12:27:45 +0300

    Introduce person.

--- hello_world/src/main/java/com/panda/helloworld/HelloWorld.java
+++ hello_world/src/main/java/com/panda/helloworld/HelloWorld.java
@@ -1,11 +1,17 @@
 package com.panda.helloworld;
 
 import com.panda.greeter.Greeter;
+import com.panda.person.Person;
 
 class HelloWorld {
   public static void main(String[] args) {
     Greeter greeter = new Greeter();
+
     System.out.println(greeter.greet());
+
+    Person person = new Person("Obama");
+    String greeting = greeter.greetTo(person);
+    System.out.println(greeting);
   }
 }
 
