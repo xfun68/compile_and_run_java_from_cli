c6b2ffdd4dc3652042ddaea2032aed5576798e1c
Xiaofeng Wang <xfun68@gmail.com>
2013-06-28 10:48:28 +0300

    DAMN: man => main

--- hello_world/src/main/java/com/panda/helloworld/HelloWorld.java
+++ hello_world/src/main/java/com/panda/helloworld/HelloWorld.java
@@ -3,7 +3,7 @@ package com.panda.helloworld;
 import com.panda.greeter.Greeter;
 
 class HelloWorld {
-  public static void man(String[] args) {
+  public static void main(String[] args) {
     Greeter greeter = new Greeter();
     System.out.println(greeter.greet());
   }
